9af907c9cf5f8692b98cb708679a1eddd8e4bf96
Gaein_nidb <mail@gaein.cn>
2021-04-16 17:06:49 +0800

    Update codes.

--- index.js
+++ index.js
@@ -4,30 +4,35 @@ const isEnabled = config.enable || !config;
 // Inject CSS for icons.
 if (isEnabled) {
 	hexo.extend.injector.register('head_end', () => {
-
 		const size = config.size;
+
+		// Values check.
 		if (size == undefined) {
 			size = 1.2;
 		}
 
+		// Add CSS in html header.
 		return `<style type="text/css">span[class="simple-icon"]{width:${size}rem;height:${size}rem;display:inline-block}span[class="simple-icon"] svg,img{display:inline-block;vertical-align:middle;height:inherit;width:inherit}</style>`;
 	}, 'default');
 }
 
-// Render tags to icons svg.
+// Render tags to icon svg.
 hexo.extend.tag.register('icon', (args) => {
 	if (!isEnabled)
+		// Plugin has not been enabled.
 		return `${icon_name} icon.`;
 
 	const icon_name = args[0];
 	if (icon_name == undefined)
+		// Throw error when lack of arguments.
 		throw new TypeError("[hexo-simpleIcon]Icon name should be string not undefined!");
 
+	// Values check.
 	if (config.type == undefined)
 		config.type = "cdn";
 	if (config.cdn_url == undefined)
-	config.cdn_url = 
+		config.cdn_url = "https://cdn.jsdelivr.net/npm/simple-icons@v4/icons/";
 
-	const func = require('./libs/convertor');
+	const func = require('./libs/convertor').default;
 	return func(icon_name, config.type, config.cdn_url);
 });
--- libs/convertor.js
+++ libs/convertor.js
@@ -1,26 +1,22 @@
-module.exports = function toIcon(
-    icon_name,
-    type = "cdn",
-    cdn_url_in = "https://cdn.jsdelivr.net/npm/simple-icons@v4/icons/") {
+export default function toIcon(icon_name, type, cdn_url) {
     // Default svg.
     const svg_content_default =
         `Here should be ${icon_name}, but we can not get icon, please check your Internet connection or the contact website master.`;
-    let icon_content = "";
+    let icon_content = "Here should be ${icon_name}, but there is a wrong type configuration.";
 
     switch ((type).toLowerCase()) {
         case "cdn":
             const icon_file = icon_name + ".svg";
-            const cdn_url = cdn_url_in;
 
+            // Set image content.
             icon_content = `<img src="${urlCombine(cdn_url, icon_file)}" />`;
-
             break;
 
         case "package":
             const icon = require(`simple-icons/icons/${icon_name}`);
-            
+
+            // Set svg content.
             icon_content = icon.get("svg");
-            
             break;
     }
 
