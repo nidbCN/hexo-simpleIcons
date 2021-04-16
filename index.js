5686975ca0c369cc06c809658a9fa2f945d0f87d
Gaein_nidb <mail@gaein.cn>
2021-04-16 11:41:17 +0800

    Add functions.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-hexo.extend.tag.register('test', function(args){
+hexo.extend.tag.register('test', function (args) {
 	console.log("HELLO!");
 	var id = args[0];
 	return 'HELLO THERE';
-  });
+});
--- libs/convertor.js
+++ libs/convertor.js
@@ -1,5 +1,16 @@
-export default toIcon(icon_name);
+export default toIcon(icon_name, options);
 
-function toIcon(icon_name) {
-    return "";
+
+
+function toIcon(icon_name, type, options) {
+    switch ((type ?? "cdn").toLowerCase()) {
+        case "cdn":
+            let cdn_url = options.cdn_url ?? "https://cdn.jsdelivr.net/npm/simple-icons@v4/icons/";
+
+            let template = `<img height="${options.height ?? 32}" width="${options.width ?? 32}" src="${cdn_url}${icon_name}.svg" />`;
+    }
+}
+
+function urlCombine(base_url, addition) {
+    return base_url + base_url.endsWith('/') ? addition : `/${addition}`;
 }
