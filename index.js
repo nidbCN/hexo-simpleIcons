5187d936c4b941a95398c912603732a33ac54c99
Gaein_nidb <mail@gaein.cn>
2021-04-16 13:46:31 +0800

    Update

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
-hexo.extend.tag.register('test', function (args) {
-	console.log("HELLO!");
-	var id = args[0];
-	return 'HELLO THERE';
-});
+if (hexo.config.simple_icons.enable || !hexo.config.simple_icons) {
+	// Inject CSS for icons.
+	hexo.extend.injector.register('head_end', () => {
+		return `<style type="text/css">span[class="simple-icon"] {width: 1.5rem;height: 1.5rem;display: inline-block;}span[class="simple-icon"] svg {display: inline-block;vertical-align: middle;height: inherit;width: inherit;}</style>`;
+	}, 'default');
+
+	// Render tags to icons svg.
+	hexo.extend.tag.register('simple-icon', (args) => {
+		let icon_name = args[0];
+		const func = require('./libs/convertor');
+		func(icon_name);
+		return 'HELLO THERE';
+	});
+}
--- libs/convertor.js
+++ libs/convertor.js
@@ -1,14 +1,33 @@
-export default toIcon(icon_name, options);
+export default toIcon(icon_name, type = "cdn", options = { cdn_url: "https://cdn.jsdelivr.net/npm/simple-icons@v4/icons/" });
 
+function toIcon(icon_name, type = "cdn", options = { cdn_url: "https://cdn.jsdelivr.net/npm/simple-icons@v4/icons/" }) {
+    // Default svg.
+    const svg_content_default =
+        `Here should be ${icon_name}, but we can not get icon, please check your Internet connection or the contact website master.`;
+    let svg_content;
 
-
-function toIcon(icon_name, type, options) {
     switch ((type ?? "cdn").toLowerCase()) {
         case "cdn":
-            let cdn_url = options.cdn_url ?? "https://cdn.jsdelivr.net/npm/simple-icons@v4/icons/";
+            const icon_file = icon_name + ".svg";
+            const cdn_url = options.cdn_url ?? "https://cdn.jsdelivr.net/npm/simple-icons@v4/icons/";
+            svg_content = getSvgContent(urlCombine(cdn_url, icon_file)) ?? svg_content_default;
+            break;
 
-            let template = `<img height="${options.height ?? 32}" width="${options.width ?? 32}" src="${cdn_url}${icon_name}.svg" />`;
+        case "package":
+            const icon = require(`simple-icons/icons/${icon_name}`);
+            svg_content = icon.get("svg") ?? svg_content_default;
+            break;
     }
+
+    return `<span id="simple-icons-${icon_name}" class="simple-icons">${svg_content}</span>`;
+}
+
+function getSvgContent(url) {
+    let req = new XMLHttpRequest();
+    req.open("GET", url);
+    req.send();
+    if (req.status === 200)
+        return req.responseText;
 }
 
 function urlCombine(base_url, addition) {
